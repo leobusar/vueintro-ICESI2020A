9fc6b27f9a59ecdb9e1b0c88463eba3b5663c57d
Leonardo <leobusta@gmail.com>
2020-03-24 18:28:47 -0500

    clase antes cuarentena

---
+++ src/components/Dog.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-img :src="dog.img" height="170px"></v-img>
+    <v-card-title>
+      <div>
+        <h3>{{ dog.name }}</h3>
+        <p>{{ dog.breed }}</p>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        dog: {
+            type: Object
+        }
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Pets.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container grid-list-md fluid>
+      <v-row>
+          <v-col cols="12" sm="4" md="3"  v-for="pet in dogs" :key="pet.breed">
+              <app-dog :dog="pet"></app-dog>
+          </v-col>
+      </v-row>
+
+  </v-container>
+</template>
+
+<script>
+import  { Dogs } from  '../config/dog';
+import Dog from '../components/Dog';
+
+export default {
+    components:{
+        appDog: Dog
+    },
+    data() {
+        return {
+            dogs: Dogs 
+        };
+    },
+};
+</script>
+
+<style>
+
+</style>
