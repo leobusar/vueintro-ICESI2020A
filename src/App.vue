bbc8cae76c3b7f1d0d01d929328a2f7586204923
Leonardo <leobusta@gmail.com>
2020-05-20 19:50:31 -0500

    fin intro

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <h3>Otro mensaje</h3>
-    <HelloWorld msg="Hello World"/>
-    <HelloWorld msg="Hola mundo"/>
-    <HelloWorld msg="Bon yourt"/>
+    <HelloWorld v-for="message in messages" :key="message.id" :msg="message.value" />
+    <input type="text" v-model="entrada" /> 
+    {{ entrada }}
+    <button @click="cambiar" >Cambio</button>
+    <div :style="{color: activeColor,  fontSize: fontSize+'px'}" >Estilo en vue</div>
   </div>
 </template>
 
@@ -14,6 +16,26 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      activeColor: "black", 
+      fontSize: 20,
+      messages: [ 
+        {id: 1, value: "Hello World"},
+        {id: 2, value: "Hola mundo"},
+        {id: 3, value: "Bon yourt"}
+         ],
+      entrada: "Ingrese algo"
+    }
+  },
+  methods: {
+    cambiar(){
+      console.log("Click en boton");
+      this.entrada = this.entrada.toUpperCase();
+      this.activeColor="green";
+      this.fontSize += 10;
+    }
   }
 }
 </script>
